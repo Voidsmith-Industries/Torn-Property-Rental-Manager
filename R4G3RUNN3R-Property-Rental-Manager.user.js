dd5b6b315a0005d6268b2f45532b25a308c82884
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-09-18 20:30:57 +0100

    Update generated userscript metadata

--- R4G3RUNN3R-Property-Rental-Manager.user.js
+++ R4G3RUNN3R-Property-Rental-Manager.user.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         R4G3RUNN3R Property Rental Manager
-// @namespace    https://github.com/R4G3RUNN3R
+// @namespace    https://github.com/Voidsmith-Industries/Torn-Property-Rental-Manager
 // @version      0.4.0
 // @description  Manage Torn rentals with truthful property/market timestamps, cache-aware cancellable scans, live diagnostics, and safe native actions.
 // @author       R4G3RUNN3R
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/Voidsmith-Industries/Torn-Property-Rental-Manager/main/R4G3RUNN3R-Property-Rental-Manager.user.js
+// @downloadURL  https://raw.githubusercontent.com/Voidsmith-Industries/Torn-Property-Rental-Manager/main/R4G3RUNN3R-Property-Rental-Manager.user.js
+// @supportURL   https://github.com/Voidsmith-Industries/Torn-Property-Rental-Manager/issues
 // @match        https://www.torn.com/properties.php*
 // @grant        GM_xmlhttpRequest
 // @connect      api.torn.com
